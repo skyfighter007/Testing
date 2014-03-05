8ead4200bfd47a87842ec814f9d69a3df56a576f
munisiiii <muneshk1@damcogroup.com>
2014-03-05 10:35:12 +0530

    update from github website 24

--- Git/Git/Global.asax.cs
+++ Git/Git/Global.asax.cs
@@ -22,9 +22,7 @@ namespace Git
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
-            AuthConfig.RegisterAuth();
-            AuthConfig.RegisterAuth();
+  
         }
     }
 }
